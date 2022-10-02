2cba7dd4b6cde6795d8aacccbb1d8f36998a8977
Abbas Ali <Abbas152052@gmail.com>
2022-10-03 04:30:32 +0500

    #

--- JS Bignning to Advance/ASYNCHRONOUS JAVASCRIPT/Asynchronous-JavaScript.js
+++ JS Bignning to Advance/ASYNCHRONOUS JAVASCRIPT/Asynchronous-JavaScript.js
@@ -335,3 +335,28 @@
 // Read data from a web server - after the page has loaded
 // Update a web page without reloading the page
 // Send data to a web server - in the background
+
+// What is AJAX?
+// AJAX = Asynchronous JavaScript And XML.
+// AJAX is not a programming language.
+// AJAX just uses a combination of:
+
+// A browser built-in XMLHttpRequest object (to request data from a web server)
+// JavaScript and HTML DOM (to display or use the data)
+// AJAX is a misleading name. AJAX applications might use XML to transport data, but it is equally common to transport data as plain text or JSON text.
+
+// AJAX allows web pages to be updated asynchronously by exchanging data with a web server behind the scenes. This means that it is possible to update parts of a web page, without reloading the whole page.
+
+// Modern Browsers (Fetch API)
+// Modern Browsers can use Fetch API instead of the XMLHttpRequest Object.
+// The Fetch API interface allows web browser to make HTTP requests to web servers.
+// If you use the XMLHttpRequest Object, Fetch can do the same in a simpler way.
+
+
+// We don't use data in XML format anymore. 
+// we use JSON now. 
+
+// we have 3 most common ways to create and send request to server
+// 1.) xmlHTTPRequest (old way of doing)
+// 2.) fetch API (new way of doing)
+// 3.) axios (this is third party library)
