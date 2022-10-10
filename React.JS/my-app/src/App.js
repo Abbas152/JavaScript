d82e52feba4d8705d7d729dd9ac212a9b170d875
Abbas Ali <Abbas152052@gmail.com>
2022-10-10 19:45:54 +0500

    #

--- React.JS/my-app/src/App.js
+++ React.JS/my-app/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Fragment } from 'react';
-console.log(Fragment);
+import Navbar from "./components/Navbar.js";
 function App() {
   return (
     <>
-    
+<Navbar/>
     </>
   );
 }
