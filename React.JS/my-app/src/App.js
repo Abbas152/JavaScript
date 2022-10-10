2e66385b6e97adeb92296559a288f3b4a98fd738
Abbas Ali <Abbas152052@gmail.com>
2022-10-10 14:24:13 +0500

    #

---
+++ React.JS/my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+console.log(Fragment);
+function App() {
+  return (
+    // JSX Fragment
+   <div className="blank">Hey Hey Boy</div>
+  );
+}
+
+export default App;
