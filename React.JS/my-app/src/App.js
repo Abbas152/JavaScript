a169897e36d8fc70eb7a67aa121e65d64de873b3
Abbas Ali <Abbas152052@gmail.com>
2022-10-13 02:33:40 +0500

    #

--- React.JS/my-app/src/App.js
+++ React.JS/my-app/src/App.js
@@ -4,24 +4,38 @@ import "./App.css";
 import { Fragment } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
+// import About from "./components/About";
+import Alert from "./components/Alert";
 function App() {
-  let [mode, setMode] = useState("light");
-  const toggleMode = ()=>{
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (AlertMessage, AlertType) => {
+    setAlert({
+      message: AlertMessage,
+      type: AlertType,
+    })
+  }
+  const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
-      document.querySelector("body").style.backgroundColor = "#2125296b";
-    }else{
+      document.body.style.backgroundColor = "#2125296b";
+      showAlert("DarkMode is Enabled Now", "success");
+    } else {
       setMode("light");
-      document.querySelector("body").style.backgroundColor = "white";
-  }
+      document.body.style.backgroundColor = "white";
+      showAlert("LightMode is Enabled Now", "success");
+    }
   }
   return (
     <>
       {/* Usually We Don't Change Props In React Function Component. */}
-      <Navbar Title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Navbar Title="TextUtils" mode={mode} toggleMode={toggleMode} />
       {/* <Navbar Title="TextUtils" About="About" /> */}
-      <TextForm heading="Enter Your Text To Analyze" mode={mode}/>
+      {/* <Alert alert={alert} /> In This Object We Pass Alert State */}
+      <Alert Alert={alert} />
+
+      <TextForm heading="Enter Your Text To Analyze" mode={mode} />
       {/* <About  /> */}
     </>
   );
