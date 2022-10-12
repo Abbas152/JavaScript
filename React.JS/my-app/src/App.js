9cec450ed2f58040578ce8a3220eda9ca976ad7e
Abbas Ali <Abbas152052@gmail.com>
2022-10-12 11:40:29 +0500

    #

--- React.JS/my-app/src/App.js
+++ React.JS/my-app/src/App.js
@@ -1,14 +1,15 @@
-
-import './App.css';
-import { Fragment } from 'react';
+import "./App.css";
+import { Fragment } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 function App() {
   return (
     <>
-    {/* Usually We Don't Change Props In React Function Component. */}
-<Navbar Title= "TextUtils" About= "About" /> 
-<TextForm heading= "Enter Your Text To Analyze"/>
+      {/* Usually We Don't Change Props In React Function Component. */}
+      <Navbar Title="TextUtils" About="About" />
+      {/* <TextForm heading="Enter Your Text To Analyze" /> */}
+      <About />
     </>
   );
 }
